1856c7b3ceae9991ceb0d35fb354ac9c580f3244
Andrew Ta <ata@impirica.tech>
2022-05-11 12:08:02 -0600

    multi input per row

--- src/Form.js
+++ src/Form.js
@@ -1,52 +1,59 @@
 import React from "react";
 
 class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { values: [] };
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      createUI(){
-         return this.state.values.map((el, i) => 
-             <div key={i}>
-                <input type="text" value={el||''} onChange={this.handleChange.bind(this, i)} />
-                <input type='button' value='remove' onClick={this.removeClick.bind(this, i)}/>
-             </div>          
-         )
-      }
-    
-      handleChange(i, event) {
-         let values = [...this.state.values];
-         values[i] = event.target.value;
-         this.setState({ values });
-      }
-      
-      addClick(){
-        this.setState(prevState => ({ values: [...prevState.values, '']}))
-      }
-      
-      removeClick(i){
-         let values = [...this.state.values];
-         values.splice(i,1);
-         this.setState({ values });
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.values.join(', '));
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-              {this.createUI()}        
-              <input type='button' value='add more' onClick={this.addClick.bind(this)}/>
-              <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+    	users: [{firstName: "", lastName: ""}]
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  addClick(){
+    this.setState(prevState => ({ 
+    	users: [...prevState.users, { firstName: "", lastName: "" }]
+    }))
+  }
+  
+  createUI(){
+     return this.state.users.map((el, i) => (
+       <div key={i}>
+    	  <input placeholder="First Name" name="firstName" value={el.firstName ||''} onChange={this.handleChange.bind(this, i)} />
+          <input placeholder="Last Name" name="lastName" value={el.lastName ||''} onChange={this.handleChange.bind(this, i)} />
+    	  <input type='button' value='remove' onClick={this.removeClick.bind(this, i)}/>
+       </div>          
+     ))
+  }
+  
+  handleChange(i, e) {
+     const { name, value } = e.target;
+     let users = [...this.state.users];
+     users[i] = {...users[i], [name]: value};
+     this.setState({ users });
+  }
+  
+  removeClick(i){
+     let users = [...this.state.users];
+     users.splice(i, 1);
+     this.setState({ users });
+  }
+  
+  handleSubmit(event) {
+    alert('A name was submitted: ' + JSON.stringify(this.state.users));
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+          {this.createUI()}        
+          <input type='button' value='add more' onClick={this.addClick.bind(this)}/>
+          <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+
 
 
 export default Form;
